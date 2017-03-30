fc6b7d0cb2c4bf49e6684bbd9699af1a3f9c7340
Ari <arianna2028@outlook.com>
2017-03-29 21:00:29 -0400

    first commit

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Main.cpp
+ *
+ *  Created on: Mar 29, 2017
+ *      Author: ari
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+
+	return 0;
+}
